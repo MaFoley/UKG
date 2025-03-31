0d613e64f84a393b50492e54bb4a87c4fcab6a2c
Malcolm Foley <mfoley.1026@gmail.com>
2025-03-30 22:52:06 -0400

    mapped the attributes, bless

--- main.py
+++ main.py
@@ -7,10 +7,25 @@ def main():
     host_url = endpoint["Base"]["base_url"]
     username = endpoint["Base"]["username"]
     password = endpoint["Base"]["password"]
-    endpoints = endpoint["Endpoints"]["endpoints"]
+    attributes = endpoint["attribute_endpoints"]
     print(f'host_url: {host_url}\n, username:{username}, password:{password}')
-    org1 = f'OrgLevel1'
-    for ep in endpoints:
+
+    print(f'trying Employee...',end="")
+    r = requests.get(f'{host_url}/Employee', auth=(username, password))
+    r.raise_for_status()
+    employee_df = pd.DataFrame.from_records(r.json()["value"], index="EmpId")
+    #Time gets special treatment
+    payload = {'$filter' : ['WorkDate ge 2025-03-22 and WorkDate le 2025-03-31']}
+    try:
+        print(f'trying Time...',end="")
+        r = requests.get(f'{host_url}/Time', auth=(username, password), params= payload)
+        print(r.url)
+        time_df = pd.DataFrame.from_records(r.json()["value"], index="Id")
+    except:
+        print("ERR")
+
+    #idColumn is the column name in the employee table
+    for ep, idColumn in attributes.items():
         try:
             print(f'trying {ep}...',end="")
             r = requests.get(f'{host_url}/{ep}', auth=(username, password))
@@ -24,18 +39,20 @@ def main():
         if r.status_code != requests.codes.ok:
             continue #ignore bad responses
         content = pd.DataFrame.from_records(r.json()["value"], index="Id")
+        content = content.rename(columns={"Name": f'{ep}_Name'
+                                ,"Description":f'{ep}_Description'
+                                }
+                                ,errors='raise')
         content.to_csv(f'.\DataFiles\{ep}.csv')
+        employee_df = employee_df.merge(content, left_on= [idColumn]
+                                        , right_index=True
+                                        )
+        time_df = time_df.merge(content, left_on= [idColumn]
+                                        , right_index=True
+                                        )
         print(content)
-    payload = {'$filter' : 'WorkDate ge 2025-03-28'}
-    #Time gets special treatment
-    try:
-        print(f'trying Time...',end="")
-        r = requests.get(f'{host_url}/Time', auth=(username, password))
-        print(r.url)
-        ts = pd.DataFrame.from_records(r.json()["value"], index="Id", params= payload)
-    except:
-        print("ERR")
-    ts.to_csv(f'Time.csv')
+    employee_df.to_csv(f'.\DataFiles\Employee.csv')
+    time_df.to_csv(f'.\DataFiles\Time.csv')
 if __name__ == "__main__":
     main()
   
