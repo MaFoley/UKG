b5b130cc3f6cb225a710778ab0d115364b532f7b
MaFoley <mfoley.1026@gmail.com>
2025-04-04 22:56:14 -0400

    Update fileDifferences.ps1

--- fileDifferences.ps1
+++ fileDifferences.ps1
@@ -1,10 +1,29 @@
+param ($directory=".\DataFiles", $logdir=".",[string[]]$properties=@(
+    'Id',
+    'Code'
+    )
+)
 
-# Import CSV files
-$File1Contents = Import-Csv -Path "Time.csv"
-$File2Contents = Import-Csv -Path "Time - Copy.csv"
- 
-# Compare the CSV files
-$ComparisonResult = Compare-Object -ReferenceObject $File1Contents -DifferenceObject $File2Contents -Property ID,EmpId,WorkDate
- 
-# Output the comparison results
-echo $comparisonResult
+$sharepath  = $logdir
+$username   = $env:USERNAME
+$hostname   = hostname
+$version    = $PSVersionTable.PSVersion.ToString()
+$datetime   = Get-Date -f 'yyyyMMddHHmmss'
+$filename   = "Transcript-${username}-${hostname}-${version}-${datetime}.txt"
+$logfile = (Join-Path -Path $sharepath -ChildPath $filename).ToString()
+
+Start-Transcript -Path $logfile -Append
+
+write-host "Begin comparison script"
+$files = Get-ChildItem -Attributes !Directory -Path $directory
+$latest2 = $files | Sort-Object -Descending -Property LastWriteTime | Select-Object -first 2
+Format-Table -InputObject $latest2 -Property Name, LastWriteTime, Extension, Length
+
+$objects = @{
+    ReferenceObject = (Import-Csv -path $latest2[1])
+    DifferenceObject = (Import-csv -path $latest2[0])
+}
+Write-Host Begin difference log:
+Compare-Object -PassThru @objects -Property $properties
+Write-Host End difference log:
+Stop-Transcript
