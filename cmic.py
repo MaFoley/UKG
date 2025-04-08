b1dcb37f7e23a753213f9743fe1f432a05270a5e
Malcolm Foley <mfoley.1026@gmail.com>
2025-04-08 16:38:56 -0400

    Cleanup files

--- cmic.py
+++ cmic.py
@@ -3,66 +3,9 @@ from pprint import pprint
 from types import SimpleNamespace
 from datetime import datetime, timedelta
 from dateutil import parser, tz
-from models import Time, Employee
+from models import Time, Employee, Timesheet_Entry
 from sqlalchemy import select
 import sqlalchemy
-class Timesheet_Entry:
-    WEEKLYPAYGROUP = 'HJRU'
-    BIWEEKLYPAYGROUP = 'HJRC'
-    WEEKLYPAYGROUP = 'HJRU'
-    BIWEEKLYPAYGROUP = 'HJRC'
-    def __init__(self, time_entry: Time):
-        """
-        TshDate expected in yyyy-mm-dd format
-        """
-        self.TshPrnCode = self._findPayRun(time_entry.paygroup.name)
-        self.TshPrnCode = self._findPayRun(time_entry.paygroup.name)
-        self.TshDate: str = parser.parse(time_entry.WorkDate).strftime('%Y-%m-%d')
-        self.TshPprYear = parser.parse(time_entry.WorkDate).year
-        self.TshPprPeriod = self._TshPprPeriod(parser.parse(time_entry.WorkDate))
-        self.TshDocumentNo = None
-        self.TshEmpNo = time_entry.employee.shortEmpId()
-        self.TshDocumentNo = None
-        self.TshEmpNo = time_entry.employee.shortEmpId()
-        self.TshTypeCode = 'J'
-        self.TshUnionCode = None
-        if time_entry.job != None:
-            self.TshTradeCode = time_entry.job.name #time->emp->jobid->job.name
-            self.TshJobdeptwoId = time_entry.project.name #time->projectid->project.name
-            self.TshphsacctwiId = time_entry.orglevel3.name
-        self.TshNormalHours: float = time_entry.RegHr
-        self.TshCompCode = time_entry.companyCode()
-        self.TshWorkCompCode = self.TshCompCode
-        self.TshcatexpId = 'L'
-        self.TshOTHours: float = time_entry.Overt1
-        self.TshDoTHours:float = time_entry.Overt2
-        self.TshWcbCode = None #TODO: attach workers comp code
-        self.TshOHType = None
-
-    def _findPayRun(self, paygroup: str) -> str:
-        if paygroup.upper().strip() == self.BIWEEKLYPAYGROUP:
-            return 'B'
-        elif paygroup.upper().strip() == self.WEEKLYPAYGROUP:
-            return 'W'
-        else:
-            return 'New Paygroup'
-    def _TshPprPeriod(self, date: datetime) -> int:
-        #if tshdate > prior period start date and <= period end date, then return pay period
-        periodEndDate = datetime(2024,12,29)
-        delta = date - periodEndDate
-        return delta.days // 14 + 1
-            
-    def __str__(self):
-        return self.__repr__()
-    def __repr__(self):
-        repr_str = f"{self.__class__.__name__}"
-        repr_str += '('
-        
-        for key, val in self.__dict__.items():
-            val       = f"'{val}'" if isinstance(val, str) else val
-            repr_str += f"{key}={val}\n, "
-        
-        return repr_str.strip(", ") + ')'
 def main():
     host_url = r'https://nova-api.cmiccloud.com/cmicprod'
     username = r'HJR||NSMITH'
